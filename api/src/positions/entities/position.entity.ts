ca7d142b25ab3f3985850d8f907ed3d197b89585
Drshv <sprig_sakura@inbox.ru>
2026-04-12 22:44:24 +0300

    add organization relation to positions

--- api/src/positions/entities/position.entity.ts
+++ api/src/positions/entities/position.entity.ts
@@ -1,9 +1,17 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Organization } from '../../organizations/entities/organization.entity';
 
 @Entity('positions')
 export class Position {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'uuid' })
+  organization_id: string;
+
+  @ManyToOne(() => Organization)
+  @JoinColumn({ name: 'organization_id' })
+  organization: Organization;
 
   @Column({ type: 'varchar', length: 255 })
   name: string;
